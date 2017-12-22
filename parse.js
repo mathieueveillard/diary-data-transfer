812df93448f21f0a6786f043f5a42338a41ac754
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2017-12-22 01:03:30 +0100

    Parse inline styles

--- parse.js
+++ parse.js
@@ -1,7 +1,55 @@
+const parse = require('xml-parser')
+
 /**
  * A function that transforms inline styles described as xml to markdown
- * 
+ * Doesn't handle bold AND italic at the same time, e.g. **_text_**
  */
-export const inlineStyleToMarkdown = function() {
-    return -1
+export const inlineStyleToMarkdown = function(xml) {
+
+    const obj = parse(xml).root
+
+    let children = obj.children.filter(node => node.name === "w:r")
+
+    children = children.map(node => {
+
+        if (node.children.length === 1
+            && node.children[0].name === "w:t") {
+            return node.children[0].content
+        }
+
+        else if (node.children.length === 2
+            && node.children[0].name === "w:rPr"
+            && node.children[1].name === "w:t") {
+
+                switch (node.children[0].children[0].name) {
+
+                    case "w:i":
+                        return "_" + node.children[1].content + "_"
+                        break
+                
+                    case "w:b":
+                        return "__" + node.children[1].content + "__"
+                        break
+                
+                    case "w:strike":
+                        return "~~" + node.children[1].content + "~~"
+                        break
+                
+                    case "w:u":
+                    default:
+                        return node.children[1].content
+                        break
+                }
+        }
+
+        else {
+            return ""
+        }
+    })
+
+    children = children.map(node => node.trim())
+        .filter(node => node !== "")
+        .join(" ")
+        
+    return children
 }
--- test/parse.test.js
+++ test/parse.test.js
@@ -1,10 +1,198 @@
-import {inlineStyleToMarkdown} from '../parse'
-var assert = require('assert')
+import {inlineStyleToMarkdown} from "../parse"
+const assert = require("assert")
 
-describe('Array', function() {
-    describe('#inlineStyleToMarkdown()', function() {
-        it('should import function', function() {
-            assert.equal(inlineStyleToMarkdown(), -1)
-        });
-    });
-});
+describe("Array", function() {
+    describe("#inlineStyleToMarkdown()", function() {
+
+        it("should ignore 'w:r' tags", function() {
+
+            const xml = `
+            <w:p w14:paraId="7E9C6B89" w14:textId="4AD3317F" w:rsidR="0047424E" w:rsidRDefault="0047424E" w:rsidP="003A75D3">
+                <w:pPr>
+                    <w:pStyle w:val="MonTweet"/>
+                </w:pPr>
+            </w:p>`
+
+            const result = inlineStyleToMarkdown(xml)
+            assert.equal(result, "")
+        })
+        
+        it("should ignore 'w:t' tags with no child tag", function() {
+
+            const xml = `
+            <w:p w14:paraId="7E9C6B89" w14:textId="4AD3317F" w:rsidR="0047424E" w:rsidRDefault="0047424E" w:rsidP="003A75D3">
+                <w:r w:rsidRPr="00EE4EF5">
+                    Le texte devrait être encapsulé dans un tag
+                </w:r>
+            </w:p>`
+
+            const result = inlineStyleToMarkdown(xml)
+            assert.equal(result, "")
+        })
+        
+        it("should ignore 'w:t' tags with more than 2 child tags", function() {
+
+            const xml = `
+            <w:p w14:paraId="7E9C6B89" w14:textId="4AD3317F" w:rsidR="0047424E" w:rsidRDefault="0047424E" w:rsidP="003A75D3">
+                <w:r w:rsidRPr="00EE4EF5">
+                    <w:rPr>
+                        <w:u w:val="single"/>
+                    </w:rPr>
+                    <w:t>un truc pas valide</w:t>
+                    <w:t>un truc pas valide</w:t>
+                </w:r>
+            </w:p>`
+
+            const result = inlineStyleToMarkdown(xml)
+            assert.equal(result, "")
+        })
+        
+        it("should handle 'w:t' tags if no inline style is associated", function() {
+
+            const xml = `
+            <w:p w14:paraId="7E9C6B89" w14:textId="4AD3317F" w:rsidR="0047424E" w:rsidRDefault="0047424E" w:rsidP="003A75D3">
+                <w:r>
+                    <w:t xml:space="preserve">Voici </w:t>
+                </w:r>
+            </w:p>`
+
+            const result = inlineStyleToMarkdown(xml)
+            assert.equal(result, "Voici")
+        })
+        
+        it("should handle 'w:t' tags with italic style", function() {
+
+            const xml = `
+            <w:p w14:paraId="7E9C6B89" w14:textId="4AD3317F" w:rsidR="0047424E" w:rsidRDefault="0047424E" w:rsidP="003A75D3">
+                <w:r w:rsidRPr="00EE4EF5">
+                    <w:rPr>
+                        <w:i/>
+                    </w:rPr>
+                    <w:t>du texte en italique</w:t>
+                </w:r>
+            </w:p>`
+
+            const result = inlineStyleToMarkdown(xml)
+            assert.equal(result, "_du texte en italique_")
+        })
+        
+        it("should handle 'w:t' tags with bold style", function() {
+
+            const xml = `
+            <w:p w14:paraId="7E9C6B89" w14:textId="4AD3317F" w:rsidR="0047424E" w:rsidRDefault="0047424E" w:rsidP="003A75D3">
+                <w:r w:rsidRPr="00EE4EF5">
+                    <w:rPr>
+                        <w:b/>
+                    </w:rPr>
+                    <w:t>du texte en gras</w:t>
+                </w:r>
+            </w:p>`
+
+            const result = inlineStyleToMarkdown(xml)
+            assert.equal(result, "__du texte en gras__")
+        })
+        
+        it("should handle 'w:t' tags with stike style", function() {
+
+            const xml = `
+            <w:p w14:paraId="7E9C6B89" w14:textId="4AD3317F" w:rsidR="0047424E" w:rsidRDefault="0047424E" w:rsidP="003A75D3">
+                <w:r w:rsidRPr="00EE4EF5">
+                    <w:rPr>
+                        <w:strike/>
+                    </w:rPr>
+                    <w:t>du texte barré</w:t>
+                </w:r>
+            </w:p>`
+
+            const result = inlineStyleToMarkdown(xml)
+            assert.equal(result, "~~du texte barré~~")
+        })
+        
+        it("should handle 'w:t' tags with underline style", function() {
+
+            const xml = `
+            <w:p w14:paraId="7E9C6B89" w14:textId="4AD3317F" w:rsidR="0047424E" w:rsidRDefault="0047424E" w:rsidP="003A75D3">
+                <w:r w:rsidRPr="00EE4EF5">
+                    <w:rPr>
+                        <w:u w:val="single"/>
+                    </w:rPr>
+                    <w:t>du texte souligné</w:t>
+                </w:r>
+            </w:p>`
+
+            const result = inlineStyleToMarkdown(xml)
+            assert.equal(result, "du texte souligné")
+        })
+        
+        it("should handle 'w:p' tags with many 'w:t' tags", function() {
+
+            const xml = `
+            <w:p w14:paraId="7E9C6B89" w14:textId="4AD3317F" w:rsidR="0047424E" w:rsidRDefault="0047424E" w:rsidP="003A75D3">
+                <w:r>
+                    <w:t xml:space="preserve">Voici </w:t>
+                </w:r>
+                <w:r w:rsidRPr="00EE4EF5">
+                    <w:rPr>
+                        <w:i/>
+                    </w:rPr>
+                    <w:t>du texte en italique</w:t>
+                </w:r>
+            </w:p>`
+
+            const result = inlineStyleToMarkdown(xml)
+            assert.equal(result, "Voici _du texte en italique_")
+        })
+        
+        it("should transform inline styles described as xml to markdown", function() {
+
+            const xml = `
+            <w:p w14:paraId="7E9C6B89" w14:textId="4AD3317F" w:rsidR="0047424E" w:rsidRDefault="0047424E" w:rsidP="003A75D3">
+                <w:pPr>
+                    <w:pStyle w:val="MonTweet"/>
+                </w:pPr>
+                <w:r>
+                    <w:t xml:space="preserve">Voici </w:t>
+                </w:r>
+                <w:r w:rsidRPr="00EE4EF5">
+                    <w:rPr>
+                        <w:i/>
+                    </w:rPr>
+                    <w:t>du texte en italique</w:t>
+                </w:r>
+                <w:r>
+                    <w:t xml:space="preserve"> et </w:t>
+                </w:r>
+                <w:r w:rsidRPr="00EE4EF5">
+                    <w:rPr>
+                        <w:b/>
+                    </w:rPr>
+                    <w:t>du texte en gras</w:t>
+                </w:r>
+                <w:r>
+                    <w:t xml:space="preserve"> ainsi que </w:t>
+                </w:r>
+                <w:r w:rsidRPr="00EE4EF5">
+                    <w:rPr>
+                        <w:u w:val="single"/>
+                    </w:rPr>
+                    <w:t>du texte souligné</w:t>
+                </w:r>
+                <w:r w:rsidRPr="00EE4EF5">
+                </w:r>
+                <w:r w:rsidRPr="00EE4EF5">
+                    <w:rPr>
+                        <w:u w:val="single"/>
+                    </w:rPr>
+                    <w:t>un truc pas valide</w:t>
+                    <w:t>un truc pas valide</w:t>
+                </w:r>
+                <w:r>
+                    <w:t xml:space="preserve">. Voilà.</w:t>
+                </w:r>
+            </w:p>`
+
+            const result = inlineStyleToMarkdown(xml)
+            assert.equal(result, "Voici _du texte en italique_ et __du texte en gras__ ainsi que du texte souligné . Voilà.")
+        })
+    })
+})
